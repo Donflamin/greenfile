41b43437464242f184939760491b9299635e3d28
Donflamin <sander.naranjo.olsson05@skola.stenungsund.se>
2023-12-19 23:39:59 +0100

    second commit

---
+++ js/global.js
@@ -0,0 +1,12 @@
+/**
+ * @file
+ * Global utilities.
+ *
+ */
+(function (Drupal) {
+  "use strict";
+
+  Drupal.behaviors.greenfile = {
+    attach: function (context, settings) {},
+  };
+})(Drupal);
