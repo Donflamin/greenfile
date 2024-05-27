215fd303fb94839440fb88442f92f65713d71121
Donflamin <sander.naranjo.olsson05@skola.stenungsund.se>
2024-05-27 04:21:28 +0200

    klar

--- js/javabript.js
+++ js/javabript.js
@@ -2,3 +2,14 @@ function toggleSearch() {
   const searchInput = document.querySelector(".search-input");
   searchInput.classList.toggle("active");
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+  document.querySelector('.scroll-down-arrow').addEventListener('click', function(e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
+
+
